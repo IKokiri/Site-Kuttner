81c9a47593d8fa38407a2453598f328c7dfb0115
Luiz Mendes <developer.luizmendes@gmail.com>
2021-07-10 15:57:12 -0300

    refactor: adição de api para dados

--- components/Timeline/Timeline.js
+++ components/Timeline/Timeline.js
@@ -5,53 +5,51 @@ function Timeline({ Component, pageProps }) {
 
     return (
         <>
-            <section class="container">
-                <div className="content-side col-md-12 col-sm-12 col-xs-12">
-                    <div className="our-blog padding-right">
-                        <div className="news-block-two">
-                            <div className="inner-box">
-                                <div className="image-box"><a href="javascript:void(0)"><img src="imgs/historia/kdbhistoria1.webp" alt="" /></a></div>
-                                <div className="lower-content clearfix">
-                                    <div className="info">
-                                        <ul>
-                                            <li><h2><i className="fa fa-clock-o"></i>Maio, 1974</h2></li>
-                                        </ul>
-                                    </div>
-                                    <div className="text">
-                                        O Grupo Küttner Alemanha se estabelece no Brasil com uma filial em Belo Horizonte para atender a crescente demanda do mercado brasileiro de indústria siderúrgica, fundição e mineração
-                                    </div>
+            {
+                historia.map((h, i) => {
 
+                    let tam = historia.length;
+
+                    let hist = <section class="container">
+                        <div className="content-side col-md-12 col-sm-12 col-xs-12">
+                            <div className="our-blog padding-right">
+                                <div className="news-block-two">
+                                    <div className="inner-box">
+                                        <div className="image-box"><a href="javascript:void(0)"><img src={h.imagem} alt="" /></a></div>
+                                        <div className="lower-content clearfix">
+                                            <div className="info">
+                                                <ul>
+                                                    <li><h2><i className="fa fa-clock-o"></i>{h.data}</h2></li>
+                                                </ul>
+                                            </div>
+                                            <div className="text">
+                                                {h.texto}
+                                            </div>
+
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </section>
+                    </section>
 
-            <FundoSlide />
-            <br/>
-            <section class="container">
-                <div className="content-side col-md-12 col-sm-12 col-xs-12">
-                    <div className="our-blog padding-right">
-                        <div className="news-block-two">
-                            <div className="inner-box">
-                                <div className="image-box"><a href="javascript:void(0)"><img src="imgs/historia/knbshistoria1.webp" alt="" /></a></div>
-                                <div className="lower-content clearfix">
-                                    <div className="info">
-                                        <ul>
-                                            <li><h2><i className="fa fa-clock-o"></i>Julho, 2017</h2></li>
-                                        </ul>
-                                    </div>
-                                    <div className="text">
-                                        A IMF Brasil é incorporada no Grupo Küttner como afiliada da Kuttner do Brasil, mudando o seu nome para Kuttner No-Bake Solutions
-                                    </div>
+                    let fundo = (tam / 2 == i + 1) ? <FundoSlide /> : ''
 
-                                </div>
-                            </div>
-                        </div>
+                    return <div>
+                        {
+                            hist
+                        }
+                        {
+
+                            fundo
+
+                        }
                     </div>
-                </div>
-            </section>
+                })
+            }
+
+
+
         </>
     )
 }
--- pages/api/historia.js
+++ pages/api/historia.js
@@ -1,93 +1,12 @@
 export const historia = [
     {
-        "titulo": 'Material Handling',
-        "ano": "1948",
-        "texto": "",
-        "imagem": "imgs/historia/materialhandling.png"
-    },
+        "data": 'Maio, 1974',
+        "texto": "O Grupo Küttner Alemanha se estabelece no Brasil com uma filial em Belo Horizonte para atender a crescente demanda do mercado brasileiro de indústria siderúrgica, fundição e mineração",
+        "imagem": "imgs/historia/kdbhistoria1.webp"
+    },  
     {
-        "titulo": "Foundation Küttner GmbH & Co. KG",
-        "ano": "1948",
-        "texto": "",
-        "imagem": "imgs/historia/kuettner.png"
-    },
-    {
-        "titulo": "Construction of Complex Furncase Feed Systems for FE & Foundry Industry",
-        "ano": "1960",
-        "texto": "",
-        "imagem": "imgs/historia/kuettner.png"
-    },
-    {
-        "titulo": "Start of Furnace Technology",
-        "ano": "1970",
-        "texto": "",
-        "imagem": "imgs/historia/startfournace.png"
-    },
-    {
-        "titulo": "Kuttner do Brasil Equipamentos Siderurgicos Ltda.",
-        "ano": "1974",
-        "texto": "",
-        "imagem": "imgs/historia/kuttnerbrasil.png"
-    },
-    {
-        "titulo": "Kuttner S.a.r.l - Küttner RSA Ltd.",
-        "ano": "1981 - 1987",
-        "texto": "",
-        "imagem": "imgs/historia/kuettner.png"
-    },
-    {
-        "titulo": "Development of Non-Ferous-Industry",
-        "ano": "1990",
-        "texto": "",
-        "imagem": "imgs/historia/development.png"
-    },
-    {
-        "titulo": "Küttner Automation",
-        "ano": "1991 - 1992",
-        "texto": "",
-        "imagem": "imgs/historia/automation.png"
-    },
-    {
-        "titulo": "Küttner LLC (USA) - Somani Küttner / India (P) Ltd.",
-        "ano": "1993 - 1995",
-        "texto": "",
-        "imagem": "imgs/historia/kuettner.png"
-    },
-    {
-        "titulo": "Development Energy and Environmental Technology",
-        "ano": "2000",
-        "texto": "",
-        "imagem": "imgs/historia/energy.png"
-    },
-    {
-        "titulo": "Küttner Energy - Kuttner Asia Co. Ltd.",
-        "ano": "2001",
-        "texto": "",
-        "imagem": "imgs/historia/kuettner.png"
-    },
-    {
-        "titulo": "Busch Brown Field Complete EPC Foundry",
-        "ano": "2013 - 2015",
-        "texto": "",
-        "imagem": "imgs/historia/bush.png"
-    },
-    {
-        "titulo": "Affilated companies:SAVELLI - TDCo Gmbh",
-        "ano": "2014 - 2016",
-        "texto": "",
-        "imagem": "imgs/historia/saveli.png"
-    },
-    {
-        "titulo": "WEG Mexico - Greensand-and No-bake - Molding Lines",
-        "ano": "2016 - 2018",
-        "texto": "",
-        "imagem": "imgs/historia/weg.png"
-    },
-    {
-        "titulo": "Kuttner No-bake Solutions",
-        "ano": "2017",
-        "texto": "",
-        "imagem": "imgs/historia/knbs.png"
-    }
-
+        "data": 'Julho, 2017',
+        "texto": "A IMF Brasil é incorporada no Grupo Küttner como afiliada da Kuttner do Brasil, mudando o seu nome para Kuttner No-Bake Solutions",
+        "imagem": "imgs/historia/knbshistoria1.webp"
+    },  
 ]
