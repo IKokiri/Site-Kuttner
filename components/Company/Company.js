598852b78816fa5518f3dcc23a07a025fc9c44dc
Luiz Mendes <developer.luizmendes@gmail.com>
2021-07-10 08:24:20 -0300

    apresentação empresas

--- components/Company/Company.js
+++ components/Company/Company.js
@@ -7,58 +7,31 @@ function Company({ Component, pageProps }) {
             {
                 company.map((c) => {
                     let conteudo = ''
-                    let texto = <div className="content-column col-md-6 col-sm-12 col-xs-12">
-                        <div className={c.classe}>
-                            <div className="inner-column">
-                                <div className="sec-title light">
-                                    <h2 style={{ color: c.corTexto }}>{c.empresa}</h2>
-                                    <h5 style={{ color: c.corTexto }}>{c.slogan}</h5>
-                                </div>
-                                <p style={{ color: c.corTexto }} className='lead text-justify'>
-                                    {c.item1}
-                                </p>
-                            </div>
+                    let texto = <div className="col-md-6">
+                        <div className="sec-title">
+                            <h2 style={{ color: c.corTexto }}>{c.empresa}</h2>
+                        </div>
+                        <div className="content-box">
+                            <h3 style={{ color: c.corTexto }}>{c.slogan}</h3>
+                            <p className="text-justify" style={{ color: c.corTexto }}>
+                                {c.item1}
+                                {conteudo}
+                            </p>
                         </div>
                     </div>
 
-                    let imagem = <div className="video-column col-md-6 col-sm-12 col-xs-12">
-                        <div className="inner-column">
-                            <figure className="image"><img src={c.item2} alt="" />
-                            </figure>
+                    let imagem = <div className="col-md-6">
+                        <div className="image-box">
+                            <img src={c.item2} alt="Awesome Image" />
                         </div>
                     </div>
 
-                    conteudo = c.posicao ? imagem : texto
-                    return <h1>Essa seção do site está em manutenção. As outras páginas estão disponíveis.</h1>
-                    return <section className="about-style-one">
+
+                    return <section style={{ backgroundColor: c.cor }} className="about-style-one">
                         <div className="auto-container">
                             <div className="row">
-                                <div className="col-md-6">
-                                    <div className="image-box">
-                                        <img src="images/resource/about-1-1.jpg" alt="Awesome Image" />
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="sec-title">
-                                        <h4>Company</h4>
-                                        <h2>About Our Company</h2>
-                                    </div>
-                                    <div className="content-box">
-                                        <h3>Our Mission</h3>
-                                        <p>Must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
-                                            and we will give you a complete account of the system, and expound the actual teachings.</p>
-                                        <h3>Our Vision</h3>
-                                        <ul className="list-items">
-                                            <li><i className="machinery-icon-favorites-button"></i> Desires to obtain pain of itself expound
-                                            </li>
-                                            <li><i className="machinery-icon-favorites-button"></i> Take a trivial example, which of us
-                                                undertakes works</li>
-                                            <li><i className="machinery-icon-favorites-button"></i> Chooses to enjoy a pleasure annoying results
-                                            </li>
-                                        </ul>
-                                        <a href="#" className="more-btn"><i className="machinery-icon-next"></i>More About Us</a>
-                                    </div>
-                                </div>
+                                {c.posicao ? texto : imagem}
+                                {c.posicao ? imagem : texto}
                             </div>
                         </div>
                     </section>
--- pages/api/company.js
+++ pages/api/company.js
@@ -3,7 +3,7 @@ export const company = [
         "posicao": false,
         "empresa": "Kuttner do Brasil",
         "item1": "Kuttner do Brasil, fornecedora tradicional de equipamentos e instalações para a indústria siderúrgica, de fundição, mineração, meio ambiente e energia, é uma empresa afiliada ao GRUPO Kuttner que tem atividades na Europa, Ásia e Américas. Fundada em 1974, com sede em Contagem – MG, atende aos mercados do Brasil e América do Sul. A combinação entre network global, know-how e experiência de mercado, faz da Kuttner do Brasil a parceira ideal para a realização de projetos industriais. A gama de atividades compreende o desenvolvimento de tecnologias e soluções técnicas avançadas, engenharia, projetos conceitual, básico e detalhado, fornecimento de equipamentos e sistemas, montagem e o start-up de instalações industriais completas com seus componentes mecânicos e elétricos, assim como, sistemas de automação e supervisórios de processos, em conformidade com as necessidades específicas de cada cliente.",
-        "item2": "imgs/empresa/kuttner do brasil.png",
+        "item2": "imgs/empresa/kuttner do brasil home.png",
         "slogan": "Interdisciplinary work. One Group.",
         "cor": "#0894d5",
         "corTexto": "#FFF",
