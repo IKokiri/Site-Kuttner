bb8be5867cd98f51eccf5e3cabef87b6508eeeed
Luiz Mendes <developer.luizmendes@gmail.com>
2021-07-10 12:18:09 -0300

    refactor: alterado banner para componente

---
+++ components/BannerHistoria/BannerHistoria.js
@@ -0,0 +1,18 @@
+function BannerHistoria({ Component, pageProps }) {
+    return (
+        <>
+            
+            <section className="page-title" style={{ backgroundImage: "url(imgs/historia/kuttnerdobrasilhistoria.webp)" }}>
+                <div className="auto-container">
+                    <h1>História</h1>
+                    <ul className="page-breadcrumb">
+                        <li></li>
+                    </ul>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default BannerHistoria
+
--- pages/historia.js
+++ pages/historia.js
@@ -1,21 +1,13 @@
 import Header from '../components/Header/Header'
 import Bootom from '../components/Bootom/Bootom';
 import Timeline from '../components/Timeline/Timeline'
-
+import BannerHistoria from '../components/BannerHistoria/BannerHistoria';
 function Historia({ Component, pageProps }) {
 
     return (
         <>
             <Header />
-
-            <section className="page-title" style={{ backgroundImage: "url(imgs/historia/kuttnerdobrasilhistoria.webp)" }}>
-                <div className="auto-container">
-                    <h1>História</h1>
-                    <ul className="page-breadcrumb">
-                        <li></li>
-                    </ul>
-                </div>
-            </section>
+            <BannerHistoria />
             <Timeline />
             <Bootom />
         </>
