9a0f0c404c884dfc49b65e4988a137189c29a08e
Luiz Mendes <developer.luizmendes@gmail.com>
2021-07-14 05:41:44 -0300

    refatoração comentários

--- components/KuttnerAlemanha/KuttnerAlemanha.js
+++ components/KuttnerAlemanha/KuttnerAlemanha.js
@@ -10,7 +10,6 @@ function KuttnerAlemanha({ Component, pageProps }) {
         src="https://www.google.com/maps/d/embed?mid=1t2BFrCIDRBbTsuDj-tBj3UXqHU2ZKaQL&z=7">
       
       </iframe>
-      {/* // src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDXSUsCy9jIv_Ehi0GR4sppGQOGdOjZgAY&q=Germany&zoom=6"> */}
     </>
   )
 }
