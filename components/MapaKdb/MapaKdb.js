ee353b21cfb3a93c79787523ecaa3af82904b9b9
Luiz Mendes <developer.luizmendes@gmail.com>
2021-08-13 16:33:09 -0300

    mapa

--- components/MapaKdb/MapaKdb.js
+++ components/MapaKdb/MapaKdb.js
@@ -1,15 +1,28 @@
 function MapaKdb({ Component, pageProps }) {
   return (
     <>
-      <iframe
-        width="100%"
-        height="900"
-        style={{border:0}}
-        loading="lazy"
-        allowfullscreen
-        src="https://www.google.com/maps/d/embed?mid=1huOIgZ8XMac7ob53RkbRy5Y67DmXeiSl&z=17">
-      
-      </iframe>
+      <style jsx>
+        .overlay {
+          position: relative;
+        width: 100%;
+        height: 450px;
+        top: 450px;
+        margin-top: -450px;
+        z-index: 1;
+}
+
+      </style>
+      <div class="overlay" onclick="style.pointerEvents='none'">
+        <iframe
+          width="100%"
+          height="900"
+          style={{ border: 0 }}
+          loading="lazy"
+          allowfullscreen
+          src="https://www.google.com/maps/d/embed?mid=1huOIgZ8XMac7ob53RkbRy5Y67DmXeiSl&z=17">
+
+        </iframe>
+      </div>
     </>
   )
 }
